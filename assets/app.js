b437d645bb09df11c08221df3698b1d87079a949
getill <98155180+getill@users.noreply.github.com>
2023-04-06 22:20:41 +0200

    🩹 Quick JS improvement

--- assets/app.js
+++ assets/app.js
@@ -7,16 +7,33 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.css";
-// import "../node_modules/jquery/src/jquery.js";
-// import "../node_modules/jquery-datetimepicker/build/jquery.datetimepicker.full.min";
 
-// // DateTimePicker
-// // jQuery.datetimepicker.setLocale("fr");
-// jQuery("#datetimepicker").datetimepicker({
-//   format: "d.m.Y H:i",
-//   inline: true,
-//   lang: "fr",
-// });
+const picker = new easepick.create({
+  element: ".datepicker",
+  css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
+  zIndex: 10,
+  lang: "fr-FR",
+  format: "DD MMMM YYYY",
+  AmpPlugin: {
+    dropdown: {
+      months: true,
+      years: true,
+      minYear: 2023,
+      maxYear: 2026,
+    },
+  },
+  LockPlugin: {
+    minDate: new Date(),
+    minDays: null,
+    maxDays: null,
+  },
+  plugins: ["AmpPlugin", "LockPlugin"],
+});
 
+// Function to log dateData when selected
+
+picker.on("select", () => {
+  console.log(picker.getDate());
+});
 // start the Stimulus application
 import "./bootstrap";
