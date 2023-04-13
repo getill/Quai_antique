4b634a9bb71b6f84e130f6c87594173fe18be232
getill <98155180+getill@users.noreply.github.com>
2023-04-13 22:18:39 +0200

    🚧 Time is now calculated dynamically
    Still trying to get the available times displayed.

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -4,26 +4,41 @@ namespace App\Controller;
 
 use App\Entity\Reservation;
 use App\Form\ReservationType;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ReservationRepository;
+use App\Entity\RestaurantWeekdayTimetable;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Repository\RestaurantWeekdayRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Repository\RestaurantWeekdayTimetableRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class ReservationController extends AbstractController
 {
     #[Route('/reservation', name: 'reservation')]
     public function reservation(RestaurantWeekdayRepository $dayRepository, RestaurantWeekdayTimetableRepository $timeRepository, Request $request, ReservationRepository $reservationrepository): Response
     {
+
         $form = $this->createForm(ReservationType::class);
         $form->handleRequest($request);
 
         // Get date
         $date = $request->get("date");
-        // dd($date);
+
+        //------------- Time creation logic ------------------
+        $array_of_time = array();
+        $openAm = $timeRepository->find('1')->getOpenAm()->getTimestamp();
+        $closeAm = $timeRepository->find('1')->getCloseAm()->getTimestamp();
+        $fifteen_mins  = 15 * 60;
+
+        while ($openAm <= $closeAm) {
+            $array_of_time[] = date("H:i", $openAm);
+            $openAm += $fifteen_mins;
+        }
+        //-------------------------------------------
+
 
         // AJAX request verification
         if ($request->get('ajax')) {
@@ -33,8 +48,10 @@ class ReservationController extends AbstractController
                     'time' => $timeRepository->findAll(),
                     'weekdays' => $dayRepository->findAll(),
                     'form' => $form->createView(),
-                    'reservationTime' => $reservationrepository->findAll()
+                    'reservationDateTime' => $reservationrepository->findAll(),
+                    'reservationTime' => $array_of_time
                 ])
+
             ]);
         }
 
@@ -43,7 +60,8 @@ class ReservationController extends AbstractController
             'time' => $timeRepository->findAll(),
             'weekdays' => $dayRepository->findAll(),
             'form' => $form->createView(),
-            'reservationTime' => $reservationrepository->findAll()
+            'reservationDateTime' => $reservationrepository->findAll(),
+            'reservationTime' => $array_of_time
         ]);
     }
 }
